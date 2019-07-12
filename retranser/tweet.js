cb5945b8028a4cb24017acedb65d7d146ae8fd7b
itanium <is0434ii@ed.ritsumei.ac.jp>
2019-07-12 14:03:13 +0900

    fix: inputのURLパラメータ作成の不具合を修正

--- retranser/tweet.js
+++ retranser/tweet.js
@@ -1,16 +1,14 @@
 function tweet(){
     let url = "https://itanium-r.github.io/retranser/index.html";
-    //url += "?input="    + ElmId("input").value;
     url += "?fromLang=" + ElmId("fromLang").value;
     url += "&toLang="   + ElmId("toLang").value;
-    url += "&input="    + ElmId("input").value.replace(" ","+");
-    //url += "&input="    + ElmId("input").value;
+    url += "&input="    + encodeURIComponent(ElmId("input").value);
     url = encodeURIComponent(url);
-    let message = document.getElementById("retranslated").value;
+    let message = ElmId("retranslated").value;
     message += "←";
-    message += document.getElementById("translated").value;
+    message += ElmId("translated").value;
     message += "←";
-    message += document.getElementById("input").value;
+    message += ElmId("input").value;
     message += encodeURIComponent( "／再翻やくん" ); //ページタイトル。同上。
 
     document.open("https://twitter.com/share?url=" + url + 
