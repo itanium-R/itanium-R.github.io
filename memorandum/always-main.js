8670b024cb29befc1c9050e7e73710257bcb14be
itanium-R <is0434ii@ed.ritsumei.ac.jp>
2019-10-19 20:00:03 +0900

    fix: split js/css from html

---
+++ memorandum/always-main.js
@@ -0,0 +1,38 @@
+
+
+    function loadIframe(page){
+        window.open(page.url,page.iframeId);
+        if(page.reloadDur > 0){
+          setInterval(()=>{window.open(page.url,page.iframeId)}, page.reloadDur*1000);
+        }
+      }
+  
+      window.onload = function(){
+        slideDur = getParam("slideDur");
+        if(!slideDur) slideDur = 10;
+  
+        var mainFrames = [];
+  
+        var body = document.getElementsByTagName("body")[0];
+  
+        for(page of pages){
+          let iframe  = document.createElement("iframe");
+          iframe.id   = iframe.name = page.iframeId;
+          iframe.className = "fullSc";
+          body.appendChild(iframe);
+          loadIframe(page);
+          mainFrames.push(document.getElementById(page.iframeId));
+        }
+        
+        new FullScSlider(mainFrames,slideDur,5);
+      };
+      
+      function getParam(name) {
+        let url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+      }
