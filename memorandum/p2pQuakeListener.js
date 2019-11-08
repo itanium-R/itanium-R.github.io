98d71d0f2f8ce70ae37a925dc2c85661e5a4cc54
itanium-R <is0434ii@ed.ritsumei.ac.jp>
2019-11-08 04:30:55 +0000

    fix: always(rits ver)に地震速報導入・ファイル分割

---
+++ memorandum/p2pQuakeListener.js
@@ -0,0 +1,184 @@
+
+      const P2PURL = "https://api.p2pquake.net/v1/human-readable?limit=1";
+      const lineCharNum = 20; // 1行の文字数
+      const CHIMEURL = "./a-chime.wav"; 
+      class p2pQuakeListener{
+        constructor(fetchDur = 3,dispDur = 2){
+          this.fetchDur = fetchDur;
+          this.dispDur  = dispDur;
+          this.init();
+          this.initQtElm();
+        }
+        run(){
+          if(this.runInterv){
+            console.log("aleady runnning.");
+            return -1;
+          }
+          if(!(1<=this.fetchDur && this.fetchDur <= 99999)){
+            console.log("invalid fetchDur");
+            return -1;
+          }
+
+          this.runInterv = setInterval(() => {
+              this.fetchP2pJson()
+            },this.fetchDur * 1000);
+        }
+
+        stop(){
+          if(this.runInterv){
+            clearInterval(this.runInterv);
+            this.runInterv = null;
+          }else{
+            console.log("not runnning.");
+          }
+        }
+
+        fetchP2pJson(){
+          fetch(P2PURL)
+          .then((response) => {
+            return response.json();
+          })
+          .then((gotJson) => {
+            this.checkP2pJsonUpdate(gotJson);
+            
+          });
+        }
+        init(){
+          this.p2pJson=[];
+          this.runInterv = null;
+        }
+        checkP2pJsonUpdate(gotJson){
+          if(this.p2pJson.length == 0){
+            this.p2pJson = gotJson;
+            console.log("first access : p2pJson got success.");
+          }else if(JSON.stringify(this.p2pJson) != JSON.stringify(gotJson)){
+            this.p2pJson = gotJson;
+            console.log("p2pJson update found.");
+            this.showQuakeInfo(gotJson[0]);
+          }else{
+            // console.log("p2pJson update is NOT found.");
+          }
+        }
+
+        initQtElm(){          
+          let qtElm = document.getElementById("quakeInfoTelop");
+          if(!qtElm){
+            qtElm = document.createElement("div");
+            qtElm.id = "quakeInfoTelop";
+            try{
+              document.getElementsByTagName("body")[0].appendChild(qtElm);
+              qtElm = document.getElementById("quakeInfoTelop");
+            }catch(e){
+              console.log("[ERROR] Invalid Html File : " + e);
+              return -1;
+            }
+          
+            qtElm.style.fontSize   = "3vw";
+            qtElm.style.fontWeight = "900";
+            qtElm.style.display    = "none";
+            qtElm.style.position   = "fixed";
+            qtElm.style.top        = "3vh";
+            qtElm.style.left       = "20vw";
+            qtElm.style.color      = "#FFF";
+            qtElm.style.webkitTextStroke = "0.15vw #235";
+            qtElm.style.textStroke       = "0.15vw #235";
+          }
+        }
+
+        parseShindo(scale){
+            switch(scale){
+              case 45:
+                return "５弱";
+              case 50: 
+                return "５強";
+              case 55:
+                return "６弱";
+              case 60: 
+                return "６強";
+              default:
+                return String.fromCharCode(String(scale / 10).charCodeAt(0) + 65248) + "　"; 
+            }
+        }
+
+        showQuakeInfo(p2pJsonNewPart){
+          this.stop();
+          try{
+            let str = "";
+            if(p2pJsonNewPart.code == 551){
+              let time    = p2pJsonNewPart.earthquake.time;
+              let maxScl  = p2pJsonNewPart.earthquake.maxScale;
+              let shindo  = this.parseShindo(maxScl);
+              let epicent = p2pJsonNewPart.earthquake.hypocenter.name;
+              let depth   = p2pJsonNewPart.earthquake.hypocenter.depth;
+              let magunit = p2pJsonNewPart.earthquake.hypocenter.magnitude;
+              let points  = p2pJsonNewPart.points;
+
+              let sclList = [70,60,55,50,45,40,30,20,10];
+
+              str += `　　　　　　■　地震速報　■<br>　<br>`
+              str += `${time}ごろ、${epicent}で<br>地震がありました。<br>`;
+              str += `震源地は${epicent}<br>`;
+              str += `深さ${depth}　マグニチュード${magunit}<br>`;
+              if(maxScl > 40 || magunit > 5){
+                str += "揺れが大きかったため、海岸付近の地域では<br>今後の津波の情報に注意してください。<br>";
+              }         
+
+              sclList.forEach(scl => {
+                let theSclAddrStr = `震度${this.parseShindo(scl)}　`;
+                points.forEach(p => {
+                  if(scl == p.scale){
+                    let theSclAddrList = theSclAddrStr.split("<br>");
+                    if(theSclAddrList[theSclAddrList.length-1].length + p.addr.length > lineCharNum){
+                      theSclAddrStr += `<br>震度${this.parseShindo(scl)}　`
+                    }
+                    theSclAddrStr += `${p.addr}　`;
+                  }
+                });
+                if(theSclAddrStr != `震度${this.parseShindo(scl)}　`){
+                  str += `${theSclAddrStr}<br>`;
+                }
+
+              });
+            }
+
+            console.log(str.replace(/<br>/g,"\n"));
+            let strList = str.split("<br>");
+            for(let i = 0;i < (strList.length);i += 2){
+              if(strList[i+1] && strList[i].slice(0,4) == strList[i+1].slice(0,4)){
+                strList[i+1] = "　　　　" + str[i].slice(4);
+              }
+            }
+            this.showTelop(strList);
+            setTimeout(() => {
+                this.run();
+              },this.dispDur*1000*(str.length+2));
+          }catch(e){
+            console.log(e);
+            this.run;
+          }
+        }
+
+        showTelop(strList){
+          document.getElementById("quakeInfoTelop").style.display = "flex";
+          this.playChime();
+          for(let i = 0;i < (strList.length);i += 2){
+            setTimeout(() => {
+              let s = strList[i] + "<br>";
+              if(strList[i+1]) s += strList[i+1];
+              document.getElementById("quakeInfoTelop").innerHTML = s;
+            },this.dispDur*1000*i);
+          }
+
+          setTimeout(() => {
+              document.getElementById("quakeInfoTelop").innerHTML = "";
+              document.getElementById("quakeInfoTelop").style.display = "none";
+              this.run();
+            },this.dispDur*1000*(strList.length));
+        }
+        
+        playChime(){
+          var audio = new Audio();
+          audio.src = CHIMEURL;
+          audio.play();
+        }
+      }
